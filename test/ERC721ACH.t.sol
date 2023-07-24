1c8a2fdbeb38e3cdfa20651a605c79246183f125
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-24 10:52:04 -0300

    I add test to verify setApproveHook on ERC721ACH.

--- test/ERC721ACH.t.sol
+++ test/ERC721ACH.t.sol
@@ -10,6 +10,7 @@ import {IBalanceOfHook} from "../src/interfaces/IBalanceOfHook.sol";
 import {IOwnerOfHook} from "../src/interfaces/IOwnerOfHook.sol";
 import {ISafeTransferFromHook} from "../src/interfaces/ISafeTransferFromHook.sol";
 import {ITransferFromHook} from "../src/interfaces/ITransferFromHook.sol";
+import {IApproveHook} from "../src/interfaces/IApproveHook.sol";
 import {IERC721ACH} from "../src/interfaces/IERC721ACH.sol";
 
 contract ERC721ACHTest is DSTest {
@@ -82,6 +83,20 @@ contract ERC721ACHTest is DSTest {
         assertEq(
             isOwner ? hook : address(0),
             address(erc721Mock.transferFromHook())
+        );
+    }
+
+    function test_approveHook(address hook, address caller) public {
+        assertEq(address(0), address(erc721Mock.approveHook()));
+        bool isOwner = caller == DEFAULT_OWNER_ADDRESS;
+        vm.prank(caller);
+        if (!isOwner) {
+            vm.expectRevert(IERC721ACH.Access_OnlyOwner.selector);
+        }
+        erc721Mock.setApproveHook(IApproveHook(hook));
+        assertEq(
+            isOwner ? hook : address(0),
+            address(erc721Mock.approveHook())
         );
     }
 
