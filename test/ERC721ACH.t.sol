dec1be06f7667e3ea865635bba0bd1064d24f6a2
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-24 11:53:27 -0300

    I init unit test for setIsApprovedForAllHook

--- test/ERC721ACH.t.sol
+++ test/ERC721ACH.t.sol
@@ -13,6 +13,7 @@ import {ITransferFromHook} from "../src/interfaces/ITransferFromHook.sol";
 import {IApproveHook} from "../src/interfaces/IApproveHook.sol";
 import {ISetApprovalForAllHook} from "../src/interfaces/ISetApprovalForAllHook.sol";
 import {IGetApprovedHook} from "../src/interfaces/IGetApprovedHook.sol";
+import {IIsApprovedForAllHook} from "../src/interfaces/IIsApprovedForAllHook.sol";
 import {IERC721ACH} from "../src/interfaces/IERC721ACH.sol";
 
 contract ERC721ACHTest is DSTest {
@@ -127,6 +128,20 @@ contract ERC721ACHTest is DSTest {
         assertEq(
             isOwner ? hook : address(0),
             address(erc721Mock.getApprovedHook())
+        );
+    }
+
+    function test_isApprovedForAllHook(address hook, address caller) public {
+        assertEq(address(0), address(erc721Mock.isApprovedForAllHook()));
+        bool isOwner = caller == DEFAULT_OWNER_ADDRESS;
+        vm.prank(caller);
+        if (!isOwner) {
+            vm.expectRevert(IERC721ACH.Access_OnlyOwner.selector);
+        }
+        erc721Mock.setIsApprovedForAllHook(IIsApprovedForAllHook(hook));
+        assertEq(
+            isOwner ? hook : address(0),
+            address(erc721Mock.isApprovedForAllHook())
         );
     }
 }
