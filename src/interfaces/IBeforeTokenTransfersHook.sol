9cf5038fcd5096e25b95c3efa09d0151c52d2c22
Bulalu <elishabulalu031@gmail.com>
2023-07-26 03:50:50 +0300

    multiline natspec 🤓

--- src/interfaces/IBeforeTokenTransfersHook.sol
+++ src/interfaces/IBeforeTokenTransfersHook.sol
@@ -5,11 +5,14 @@ pragma solidity ^0.8.15;
 /// @dev Interface that defines hooks to be executed before token transfers.
 interface IBeforeTokenTransfersHook {
 
-    /// @notice Emitted when the before token transfers hook is used.
-    /// @param from Address from which the tokens are being transferred.
-    /// @param to Address to which the tokens are being transferred.
-    /// @param startTokenId The starting ID of the tokens being transferred.
-    /// @param quantity The number of tokens being transferred.
+    /**
+        @notice Emitted when the before token transfers hook is used.
+        @param from Address from which the tokens are being transferred.
+        @param to Address to which the tokens are being transferred.
+        @param startTokenId The starting ID of the tokens being transferred.
+        @param quantity The number of tokens being transferred.
+    
+     */
     event BeforeTokenTransfersHookUsed(
         address from,
         address to,
@@ -17,12 +20,15 @@ interface IBeforeTokenTransfersHook {
         uint256 quantity
     );
 
-    /// @notice Checks if the token transfers function should use the custom hook.
-    /// @param from Address from which the tokens are being transferred.
-    /// @param to Address to which the tokens are being transferred.
-    /// @param startTokenId The starting ID of the tokens being transferred.
-    /// @param quantity The number of tokens being transferred.
-    /// @return A boolean indicating whether or not to use the custom hook for the token transfers function.
+    /**
+        @notice Checks if the token transfers function should use the custom hook.
+        @param from Address from which the tokens are being transferred.
+        @param to Address to which the tokens are being transferred.
+        @param startTokenId The starting ID of the tokens being transferred.
+        @param quantity The number of tokens being transferred.
+        @return A boolean indicating whether or not to use the custom hook for the token transfers function.
+        
+     */
     function useBeforeTokenTransfersHook(
         address from,
         address to,
@@ -30,11 +36,14 @@ interface IBeforeTokenTransfersHook {
         uint256 quantity
     ) external view returns (bool);
 
-    /// @notice Provides a custom implementation for the token transfers process.
-    /// @param from Address from which the tokens are being transferred.
-    /// @param to Address to which the tokens are being transferred.
-    /// @param startTokenId The starting ID of the tokens being transferred.
-    /// @param quantity The number of tokens being transferred.
+    /**
+        @notice Provides a custom implementation for the token transfers process.
+        @param from Address from which the tokens are being transferred.
+        @param to Address to which the tokens are being transferred.
+        @param startTokenId The starting ID of the tokens being transferred.
+        @param quantity The number of tokens being transferred.
+            
+     */
     function beforeTokenTransfersOverrideHook(
         address from,
         address to,
